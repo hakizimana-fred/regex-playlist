031b616e78fa2bbbc4573f868e3566cc7199beb6
hakizimana-fred <hakifred20@gmail.com>
2021-05-03 16:52:41 +0300

    phone field

--- validation.js
+++ validation.js
@@ -1,7 +1,22 @@
 // validation script here
+const inputs = document.querySelectorAll('input')
 const reg = /[a-zA-Z]/gi
 
 const patterns = {
     telephone: /^\d{10}$/
 
 }
+
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid'
+    } else {
+        field.className = 'invalid'
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
