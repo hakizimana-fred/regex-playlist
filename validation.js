c553dcd3b58d18db8d6476fb8f4a27406529515c
hakizimana-fred <hakifred20@gmail.com>
2021-05-03 17:42:36 +0300

    final

--- validation.js
+++ validation.js
@@ -6,7 +6,8 @@ const patterns = {
     telephone: /^\d{10}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
-    slug: /^[a-z\d-]{8,20}$/
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 
 }
 
